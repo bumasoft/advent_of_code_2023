bc502fce9472bfb0b760ddaf50e8d7f1a4cce463
Marian Busoi <koin.marian@protonmail.com>
2023-12-04 00:22:52 +0200

    day 3 c: new comments

--- day_3/c/adjacent.c
+++ day_3/c/adjacent.c
@@ -3,6 +3,9 @@
 //
 #include "adjacent.h"
 
+/*
+ * Checks the left, right, top, top left and top right for an adjacent symbol.
+ */
 bool cell_is_adjacent_to_symbol(char *line, char *prev_line, size_t i) {
     if (IS_SYMBOL(line[i + 1]) || (i > 0 && IS_SYMBOL(line[i - 1]))) return true;
     if (!prev_line[i]) return false;
@@ -10,6 +13,9 @@ bool cell_is_adjacent_to_symbol(char *line, char *prev_line, size_t i) {
     return (IS_SYMBOL(prev_line[i]) || (i > 0 && IS_SYMBOL(prev_line[i - 1])) || IS_SYMBOL(prev_line[i + 1]));
 }
 
+/*
+ * Checks the top, top left and top right for an adjacent number.
+ */
 bool cell_is_adjacent_to_num_above(char *prev_line, size_t i) {
     if (!prev_line[i]) return false;
 
