a24dd5413e427e7b768217066cda68541319b434
Marian Busoi <koin.marian@protonmail.com>
2023-12-06 14:27:24 +0200

    remove unnecessary files

--- day_5/c/part_one.h
+++ day_5/c/part_one.h
@@ -5,7 +5,6 @@
 #ifndef C_PART_ONE_H
 #define C_PART_ONE_H
 #include "bumasoft/standard.h"
-#include "num_cell.h"
 #include "aoc.h"
 
 void solve_part_one(FILE* fp, vector_t* seeds, solution_t* solution);
