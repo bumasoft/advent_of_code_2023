8585bd296360a3842def39dfdfe82f898169d053
koin-marian <85279048+koin-marian@users.noreply.github.com>
2023-12-14 12:11:27 +0200

    day 10 part 1 refactor

--- day_10/c/solution.c
+++ day_10/c/solution.c
@@ -4,8 +4,6 @@
 
 #include "solution.h"
 #include "bumasoft/math.h"
-#include <stdio.h>
-#include <string.h>
 
 void part_one_recurse(tile_t **maze, size_t row_count, size_t col_count, position_t start) {
     if (start.x > col_count - 1 || start.x < 0 || start.y < 0 || start.y > row_count - 1) return;
@@ -22,7 +20,7 @@ void part_one_recurse(tile_t **maze, size_t row_count, size_t col_count, positio
         if (next_pos.x > col_count - 1 || next_pos.x < 0 || next_pos.y > row_count - 1 || next_pos.y < 0) continue;
 
         uint8_t mirror_mask = (1 << MIRROR_DIR(DIRECTIONS[i]));
-        maze[next_pos.y][next_pos.x].status |= mirror_mask; // set bottom status flag
+        maze[next_pos.y][next_pos.x].status |= mirror_mask; // set opposite direction status flag
 
         if (CAN_GO(this_pipe, DIRECTIONS[i])) {
             uint32_t next_dist = maze[next_pos.y][next_pos.x].dist == 0 ? maze[start.y][start.x].dist + 1
@@ -30,7 +28,7 @@ void part_one_recurse(tile_t **maze, size_t row_count, size_t col_count, positio
             maze[next_pos.y][next_pos.x].dist = IS_START_OR_DIRT(maze[next_pos.y][next_pos.x].symbol) ? 0 : next_dist;
 
             part_one_recurse(maze, row_count, col_count, next_pos);
-            maze[next_pos.y][next_pos.x].status ^= mirror_mask;
+            maze[next_pos.y][next_pos.x].status ^= mirror_mask; // clear opposite direction status flag
         }
     }
 
